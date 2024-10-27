b692df1813c841becf8df8e40eecdb6ed218f3cd
satyajit <satyajit_jbl@yahoo.com>
2024-10-27 22:11:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-7xl bg-red-300'>Vite + React</h1>
+
+      
+     
+    </>
+  )
+}
+
+export default App
